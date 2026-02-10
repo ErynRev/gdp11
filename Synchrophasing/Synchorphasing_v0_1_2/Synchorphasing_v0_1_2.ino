bf1bf01a1054b039e26169f1f70ba8d65480c28d
ErynRev <erynsosocial@gmail.com>
2026-02-10 14:07:52 +0000

    Corrected mistakes using Arduino IDE that dont show here modified:   Synchrophasing/Synchorphasing_v0_1_2/Synchorphasing_v0_1_2.ino

--- Synchrophasing/Synchorphasing_v0_1_2/Synchorphasing_v0_1_2.ino
+++ Synchrophasing/Synchorphasing_v0_1_2/Synchorphasing_v0_1_2.ino
@@ -29,13 +29,13 @@ typedef struct{
     signed int RPMreadings[3];
 }Prop;
 
-Prop add_prop(Prop* p, int CSN, int PWM, unsigned int phase, signed int rpm, signed int RPMreadings[3]) {
+Prop add_prop(Prop* p, int CSN, int PWM, signed int phase, unsigned int rpm, unsigned int RPMreadings[3]) {
     p->CSN = CSN;
     p->PWM = PWM;
     p->phase = phase;
     p->rpm = rpm;
     for(int i = 0; i < 3; i++) {
-        p->RPMreadings[i] = RPMreading;
+        p->RPMreadings[i] = RPMreadings[i];
     }
 }
 
@@ -46,44 +46,47 @@ signed int RPMSensor(Prop* p, int CSN) {
 
 //Send the Command Frame
     int pos[2];
-    int angle[2]
+    int angle[2];
+    unsigned long StartTime = micros();
     
     for(int i = 0; i<2; i++) {
         //Send the Command Frame
-        digitalWrite(CSN[i], LOW);
+        digitalWrite(CSN, LOW);
         delayMicroseconds(1);
         SPI.transfer16(0xFFFF);
-        digitalWrite(CSN[i],HIGH);
+        digitalWrite(CSN,HIGH);
         
         //Read data frame
-        digitalWrite(CSN[i], LOW);
+        digitalWrite(CSN, LOW);
 
         delayMicroseconds(1);
 
-        angle = SPI.transfer16(0xC000);
-            
+        angle[i] = SPI.transfer16(0xC000);
+        
 
         digitalWrite(CSN, HIGH);
         
         SPI.endTransaction();
         
-        angle[i] = (angle & (0x3FFF)); // Get Angle
+        angle[i] = (angle[i] & (0x3FFF)); // Get Angle
 
         delayMicroseconds(5);
     }
+    unsigned long CurrentTime = micros();
+    unsigned long ElapsedTime = (CurrentTime - StartTime - 5); // minus extra 5 from end delay
     // calc pos outside of run 
     for(int i = 0; i < 2; i++) {
     pos[i] = ( (unsigned long) angle[i])*360UL/16384UL;
     }
      // in total between read times 
       
-    return (signed int) ((pos[2]-pos[1])/(0.007)); // Positions Difference / Over time taken 
+    return (signed int) ((pos[2]-pos[1])/(ElapsedTime)); // Positions Difference / Over time taken 
     
     // unsure if this works tbh
 
 }
 
-signed int AVERAGERPM(Prop* p, int CSN) {
+signed int AverageRPM(Prop* p, int CSN) {
     // cant trust that the above was completely reliable especially cus unsure how quick code will run
     int rpmaverage = 0;
 
@@ -103,22 +106,22 @@ signed int AVERAGERPM(Prop* p, int CSN) {
 
 
 
-signed int ESCtoRPM_readings[3](Prop* p, int CSN, int PWM) { // Returns RPMreadings[3] for each prop
+unsigned int ESCtoRPM_readings(Prop* p, int CSN, int PWM) { // Returns RPMreadings[3] for each prop
     // This converts the ESC to RPM using the readings of the specific Motor
     // Initialises at 3 points to get a rough linear approximation for the ESC to RPM
     // Take three Throttle values between 1000 and 2000 (min and max PWM for )
     
     // Using values 1200, 1500, 1800
     int PWMvalues[3] = {1200, 1500, 1800};
-    signed int Readings[3];
+    unsigned int Readings[3];
 
     for(int i = 0; i < 3; i++){
         
         digitalWrite(PWM, PWMvalues[i]);
         delay(1000); // delay for a second for warm up
-        Readings[i] = AverageRPM(p, CSN) // (calling function for finding RPM);
+        Readings[i] = AverageRPM(p, CSN); // (calling function for finding RPM);
     }
-    return Readings[3];
+    return (Readings[3]);
 
 
 }
@@ -129,14 +132,15 @@ void setup() {
     // INITIALISATION OF PROPS 
     Prop props[2];
     // Set CSN, PWM pins, and rpms and phase to start at zero;
-    add_prop(&props[0], 10, 11, 0, 0, {0, 0, 0});
-    add_prop(&props[0], 15, 16, 0, 0, {0, 0, 0});
+    unsigned int RPMReads[3] = {0,0,0}; // start all at 0 
+    add_prop(&props[0], 10, 11, 0, 0, RPMReads);
+    add_prop(&props[1], 15, 16, 0, 0, RPMReads);
     
 
     // SETUP Pins
 
     
-    for(int i = 0; i < 2, i++>) {
+    for(int i = 0; i < 2; i++) {
         //Set Pin Modes
         pinMode(props[i].CSN, OUTPUT);
         pinMode(props[i].PWM, OUTPUT);
@@ -151,12 +155,12 @@ void setup() {
     SD.begin();
 
     SD.exists("datalog.txt");
-    datalog = SD.open("datalog.txt", FILE_WRITE);
+    //datalog = SD.open("datalog.txt", FILE_WRITE);
 
     // Initialise RPM and ESC Conversion for Calc later
     for(int i = 0; i< 2; i++) {
         for(int j = 0; i<3; j++){
-            props[i].RPMreadings[j] = ESCtoRPM_readings[j](&props[i], &props[i].CSN, &props[i].PWM)
+            props[i].RPMreadings[j] = ESCtoRPM_readings(&props[i], &props[i].CSN, &props[i].PWM);
         }
     }
     
@@ -178,8 +182,8 @@ void loop() {
                 // NEEDS to be < 1Hz out (60rpm)
                 if(rpm[i] < (rpm[0] - 30)) {
                     PWM[i] += 10; 
-                    /*whatever this uses change by incremental steps, preferably large enough
-                    that it doesnt take forever, BUT not too big that it can not fine adjust within.
+                    // whatever this uses change by incremental steps, preferably large enough
+                    // that it doesnt take forever, BUT not too big that it can not fine adjust within.
                     
                     
                     
